4673e5f5d42087994ad8096ff4bfdc76cf88bc2f
Ryannathan Setiawan <leonheart94@gmail.com>
2015-01-23 15:31:57 +0700

    pisah controller, model, ganti routes

---
+++ application/controllers/material.php
@@ -0,0 +1,21 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of material
+ *
+ * @author Ryannathan
+ */
+class Material extends CI_Controller {
+    
+    function __construct() {
+        parent::__construct();
+        $this->load->helper('url');
+    }
+    //put your code here
+}
---
+++ application/controllers/member.php
@@ -0,0 +1,20 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of member
+ *
+ * @author Ryannathan
+ */
+class Member extends CI_Controller {
+    //put your code here
+    function __construct() {
+        parent::__construct();
+        $this->load->helper('url');
+    }
+}

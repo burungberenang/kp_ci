563b13e604bb44fa9a24d4b2cfadfb6e4e9162cd
burungberenang <burungberenang@gmail.com>
2015-06-11 02:07:07 +0700

    Selesai sudah semuanya

--- application/views/front/f_body.php
+++ application/views/front/f_body.php
@@ -26,64 +26,66 @@
             <span class="sr-only">Next</span>
         </a>
     </div><!-- /.carousel -->
-        
+
     <div class="container marketing">
 
-      <!-- Three columns of text below the carousel -->
-      <div class="row">
-        <div class="col-lg-4">
-          <h2>Paket terlaris</h2>
-          <ol style="text-align: left">
-            <?php foreach ($produk as $value): ?>
-              <li><h4><?php echo $value['materi2'].' '.$value['materi1']; ?></h4></li>
-              <p><?php echo $value['nama']?></p>
-            <?php endforeach; ?>
-          </ol>
-        </div><!-- /.col-lg-4 -->
-        <div class="col-lg-4">
-          <h2>Cara pembelian</h2>
-          <ol style="text-align: left">
-              <li><p>Daftar/Masuk terlebih dahulu</p></li>
-              <li><p>Pilih <a href="<?php echo site_url(); ?>/materi/beli">Beli Materi</a></p></li>
-              <li><p>Klik "Tambahkan" pada paket yang diinginkan</p></li>
-              <li><p>Setelah selesai, masuk ke <a href="<?php echo site_url(); ?>/materi/cart">Shopping Cart <span class="glyphicon glyphicon-shopping-cart"></span></a></p></li>
-              <li><p>Klik tombol "Checkout" pada shopping cart</p></li>
-              <li><p>Pada halaman Checkout, klik tombol "Ok". Setelah itu lakukan transfer seperti yang diminta dan scan/foto/screenshot bukti transfer</p></li>
-              <li><p>Pesanan akan masuk ke halaman <a href="<?php echo site_url(); ?>/materi/bayar">Konfirmasi</a></p></li>
-              <li><p>Klik tombol "Bayar" pada pesanan yang mau dibayarkan</p></li>
-              <li><p>Upload file bukti transfer pada halaman tersebut</p></li>
-              <li><p>Tunggu konfirmasi dari admin</p></li>
-              <li><p>Jika sudah terkonfirmasi, dapat melihat paket di menu <a href="<?php echo site_url(); ?>/materiku">Materiku</a></p></li>
-          </ol>
-        </div><!-- /.col-lg-4 -->
-        <div class="col-lg-4">
-            <h2>Hubungi kami</h2>
-            <?php if(validation_errors()) echo '<div class="alert alert-danger" role="alert">'.validation_errors().'</div>'; ?>
+        <!-- Three columns of text below the carousel -->
+        <div class="row">
+            <div class="col-lg-4">
+                <h2>Paket terlaris</h2>
+                <ol style="text-align: left">
+                    <?php foreach ($produk as $value): ?>
+                        <li><h4><?php echo $value['materi2'] . ' ' . $value['materi1']; ?></h4></li>
+                        <p><?php echo $value['nama'] ?></p>
+                    <?php endforeach; ?>
+                </ol>
+            </div><!-- /.col-lg-4 -->
+            <div class="col-lg-4">
+                <h2>Cara pembelian</h2>
+                <ol style="text-align: left">
+                    <li><p>Daftar/Masuk terlebih dahulu</p></li>
+                    <li><p>Pilih <a href="<?php echo site_url(); ?>/materi/beli">Beli Materi</a></p></li>
+                    <li><p>Klik "Tambahkan" pada paket yang diinginkan</p></li>
+                    <li><p>Setelah selesai, masuk ke <a href="<?php echo site_url(); ?>/materi/cart">Shopping Cart <span class="glyphicon glyphicon-shopping-cart"></span></a></p></li>
+                    <li><p>Klik tombol "Checkout" pada shopping cart</p></li>
+                    <li><p>Pada halaman Checkout, klik tombol "Ok". Setelah itu lakukan transfer seperti yang diminta dan scan/foto/screenshot bukti transfer</p></li>
+                    <li><p>Pesanan akan masuk ke halaman <a href="<?php echo site_url(); ?>/materi/bayar">Konfirmasi</a></p></li>
+                    <li><p>Klik tombol "Bayar" pada pesanan yang mau dibayarkan</p></li>
+                    <li><p>Upload file bukti transfer pada halaman tersebut</p></li>
+                    <li><p>Tunggu konfirmasi dari admin</p></li>
+                    <li><p>Jika sudah terkonfirmasi, dapat melihat paket di menu <a href="<?php echo site_url(); ?>/materiku">Materiku</a></p></li>
+                </ol>
+            </div><!-- /.col-lg-4 -->
+            <div class="col-lg-4">
+                <h2>Hubungi kami</h2>
+                <?php if (validation_errors()) echo '<div class="alert alert-danger" role="alert">' . validation_errors() . '</div>'; ?>
 
-            <?php if ($this->session->flashdata('warning'))
-              {  echo $this->session->flashdata('warning'); }?>
-            <form action="<?php echo site_url(); ?>/contact" class="form-horizontal" method="post">
-                <div class="form-group">
-                    <label class="col-sm-3 control-label">Nama Lengkap</label>
-                    <div class="col-sm-9">
-                      <input type="text" class="form-control" name="nama" placeholder="Nama Lengkap" required>
+                <?php if ($this->session->flashdata('warning')) {
+                    echo $this->session->flashdata('warning');
+                }
+                ?>
+                <form action="<?php echo site_url(); ?>/contact" class="form-horizontal" method="post">
+                    <div class="form-group">
+                        <label class="col-sm-3 control-label">Nama Lengkap</label>
+                        <div class="col-sm-9">
+                            <input type="text" class="form-control" name="nama" placeholder="Nama Lengkap" required>
+                        </div>
                     </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-sm-3 control-label">Email</label>
-                    <div class="col-sm-9">
-                      <input type="email" class="form-control" name="email" placeholder="Email" required>
+                    <div class="form-group">
+                        <label class="col-sm-3 control-label">Email</label>
+                        <div class="col-sm-9">
+                            <input type="email" class="form-control" name="email" placeholder="Email" required>
+                        </div>
                     </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-sm-3 control-label">Pesan</label>
-                    <div class="col-sm-9">
-                        <textarea class="form-control" name="pesan" placeholder="Pesan" required></textarea>
+                    <div class="form-group">
+                        <label class="col-sm-3 control-label">Pesan</label>
+                        <div class="col-sm-9">
+                            <textarea class="form-control" name="pesan" placeholder="Pesan" required></textarea>
+                        </div>
                     </div>
-                </div>
-                <input type="submit" name="submit" value="Kirim" class="btn btn-primary">
-            </form>
-        </div><!-- /.col-lg-4 -->
-      </div>
+                    <input type="submit" name="submit" value="Kirim" class="btn btn-primary">
+                </form>
+            </div><!-- /.col-lg-4 -->
+        </div>
     </div>
 </div>
